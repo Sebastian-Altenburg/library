b46c056d7826873a76ad71fd12d9a95cc2d7d1ab
sebastian-altenburg <saltenburg90@gmail.com>
2020-05-21 10:18:28 +0200

    added checkBoxRead and deleteButton without any function yet

--- script.js
+++ script.js
@@ -1,38 +1,32 @@
 
-/// Global Variables
-let wholeContainer = document.querySelector(".wholeContainer");
-
+/// Global Variables//
 let myLibrary=[];
 let addButton = document.querySelector("button");
+let deleteButton;
+///////////////////
 
+// Selectors for parent Divs
 let parentTitle = document.querySelector(".gridTitle");
 let parentAuthor = document.querySelector(".gridAuthor");
 let parentPages = document.querySelector(".gridPages");
-let checkBox = document.querySelector(".checkboxRead");
-///
-
+let parentAlreadyRead = document.querySelector(".gridRead");
+let parentDeleteButton = document.querySelector(".parentDelete");
+///////////////////////////
 function Book(title, author, pages) {
   this.title = title;
   this.author = author;
   this.pages = pages;
 }
-
-/// Check if you read the book////
-// if (checkBox.checked == true) {
-    
-// }else{
-    
-// }
-
-
-//////////////////////
+////////////////////////
 addButton.addEventListener("click", ()=> {
+    //Pick input values and push them into the library Array
     let bookTitle = document.getElementsByClassName("bookTitle")[0].value;
     let bookAuthor = document.getElementsByClassName("bookAuthor")[0].value;
     let bookPages = document.getElementsByClassName("bookPages")[0].value;
-
     addBookToLibrary(new Book(bookTitle, bookAuthor, bookPages));
-
+    ///////////////////
+ 
+////////Create different children for the Books
     for (let key in myLibrary) {
         let childTitle = document.createElement("div");
         parentTitle.appendChild(childTitle);
@@ -45,21 +39,35 @@ addButton.addEventListener("click", ()=> {
         let childPages = document.createElement("div");
         parentPages.appendChild(childPages);
         render(myLibrary[key].pages, childPages);
+
+        let childAlreadyRead = document.createElement("div");
+        let checkboxRead = document.createElement("INPUT");
+        parentAlreadyRead.appendChild(childAlreadyRead);
+        childAlreadyRead.appendChild(checkboxRead);
+        checkboxRead.setAttribute("type", "checkbox");
+        checkboxRead.classList.add("checkboxSize");
+
+        let childDelete = document.createElement("div");
+        deleteButton = document.createElement("input");
+        deleteButton.setAttribute("type", "button");
+        deleteButton.classList.add("childDeleteButton");
+
+        parentDeleteButton.appendChild(childDelete);
+        childDelete.appendChild(deleteButton);
     }
     removeDivs();
-    
 });
-
 function addBookToLibrary(addBook) {
     myLibrary.push(addBook);
 }
 function removeDivs() {
     if (parentTitle.hasChildNodes()) {
-        console.log("hi");
         for(i = 0; i < parentTitle.children.length; i++) {
             parentTitle.removeChild(parentTitle.children[i]);
             parentAuthor.removeChild(parentAuthor.children[i]);
             parentPages.removeChild(parentPages.children[i]);
+            parentAlreadyRead.removeChild(parentAlreadyRead.children[i]);
+            parentDeleteButton.removeChild(parentDeleteButton.children[i]);
         }
     }
 }
@@ -71,5 +79,23 @@ function render(template, node) {
 
 
 
+/// Need to be implemented!!!!///////////////////////////////////////////////////////////////////
 
 
+/////////////Delete book
+// deleteButton.addEventListener("click", ()=> {
+
+// });
+//////////////////////
+
+   //Check if you read the book
+/* <input class="checkboxRead" type="checkbox"></input> */
+    // if (checkBox.checked == true) {
+    //     alreadyRead = true;
+    // }else if (checkBox.checked == false){
+    //     alreadyRead = false;
+    // }
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////

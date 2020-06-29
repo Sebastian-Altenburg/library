e8aad9d2185a8402e180a3e96442801eafcfa13b
sebastian-altenburg <saltenburg90@gmail.com>
2020-06-29 21:02:34 +0200

    function renderNew to put the form data into the table

--- script.js
+++ script.js
@@ -21,15 +21,17 @@ let pagesInput = document.querySelector("#pages");
 let addButton = document.querySelector("#add");
 let resetButton = document.querySelector("#reset");
 
+//
 let tableWhole = document.querySelector("table");
 let checkBox = document.querySelector("#checkbox");
 
 
 /////// Store the typedin values from the form to the array
 addButton.addEventListener("click", ()=>{
-  titleValue = titleInput.value;
-  authorValue = authorInput.value;
-  pagesValue = pagesInput.value;
+
+  let titleValue = titleInput.value;
+  let authorValue = authorInput.value;
+  let pagesValue = pagesInput.value;
 
   let checkBoxValue;
   if (checkBox.checked == true) {
@@ -39,10 +41,38 @@ addButton.addEventListener("click", ()=>{
     checkBoxValue = false;
   }
 
-  addBookToLibrary(new Book(titleValue, authorValue, pagesValue, checkBoxValue)); 
+  addBookToLibrary(titleValue, authorValue, pagesValue, checkBoxValue); 
+  
 });
 
 
-function addBookToLibrary(newBook) {
+function addBookToLibrary(title, author, pages, alreadyRead) {
+
+  let newBook = new Book(title, author, pages, alreadyRead);
   myLibrary.push(newBook);
+  renderNew(newBook);
+}
+
+
+// Create tableRows and tableData + deleteButton
+function renderNew(element) {
+
+  let itemIndex = myLibrary.indexOf(element);
+
+  let tableRow = document.createElement("tr");
+  tableRow.setAttribute("data-index", `${itemIndex}`);
+  tableWhole.appendChild(tableRow);
+
+  for(j = 0; j< 5; j++ ){
+    let tableData = document.createElement("td");
+    tableRow.appendChild(tableData);
+
+  }
+
+  for(i = 0; i < myLibrary.length; i++ ){
+    tableWhole.children[i+2].children[0].innerHTML = myLibrary[i].title;
+    tableWhole.children[i+2].children[1].innerHTML = myLibrary[i].author;
+    tableWhole.children[i+2].children[2].innerHTML = myLibrary[i].pages;
+    tableWhole.children[i+2].children[3].innerHTML = myLibrary[i].alreadyRead;
+  }
 }

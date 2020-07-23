2fb86d75f7ca0938db920a1d4288b65a76611ec6
sebastian-altenburg <saltenburg90@gmail.com>
2020-07-23 17:29:38 +0200

    delete problem solved, form shows up when add is clicked, click outside of form to close it, cleaner and simpler code

--- script.js
+++ script.js
@@ -3,15 +3,15 @@
 let myLibrary = new Array();
 
 // Prototype function array, deleting empty entries
-Array.prototype.clean = function(deleteValue) {
-  for (let i = 0; i < this.length; i++) {
-    if (this[i] == deleteValue) {         
-      this.splice(i, 1);
-      i--;
-    }
-  }
-  return this;
-};
+// Array.prototype.clean = function(deleteValue) {
+//   for (let i = 0; i < this.length; i++) {
+//     if (this[i] == deleteValue) {         
+//       this.splice(i, 1);
+//       i--;
+//     }
+//   }
+//   return this;
+// };
 
 // Constructor
 function Book(title, author, pages, alreadyRead){
@@ -31,15 +31,22 @@ let pagesInput = document.querySelector("#pages");
 //Buttons
 let addButton = document.querySelector("#add");
 let resetButton = document.querySelector("#reset");
+let submitButton = document.querySelector("#submit");
 
 //
 let tableWhole = document.querySelector("table");
+let formWhole = document.querySelector("form");
+let body = document.querySelector("body");
 let checkBox = document.querySelector("#checkbox");
 
 
-// Store the typedin values from the form to the array
+// open up the form
 addButton.addEventListener("click", ()=>{
-
+  formWhole.classList.remove("hide");
+  formWhole.classList.add("show");
+});
+// Store the typedin values from the form to the array
+submitButton.addEventListener("click", ()=>{
   let titleValue = titleInput.value;
   let authorValue = authorInput.value;
   let pagesValue = pagesInput.value;
@@ -51,11 +58,8 @@ addButton.addEventListener("click", ()=>{
   else if (checkBox.checked == false) {
     checkBoxValue = false;
   }
-
   addBookToLibrary(titleValue, authorValue, pagesValue, checkBoxValue); 
-  
 });
-
 
 function addBookToLibrary(title, author, pages, alreadyRead) {
 
@@ -64,50 +68,55 @@ function addBookToLibrary(title, author, pages, alreadyRead) {
   renderNew(newBook);
 }
 
-
 // Create tableRows and tableData + deleteButton
 function renderNew(element) {
 
-  let itemIndex = myLibrary.indexOf(element);
-
   let tableRow = document.createElement("tr");
+  let itemIndex = myLibrary.indexOf(element);
   tableRow.setAttribute("data-index", `${itemIndex}`);
   tableWhole.appendChild(tableRow);
 
-  for(j = 0; j< 5; j++ ){
+  for (let key in element) {
     let tableData = document.createElement("td");
+    tableData.textContent = element[key];
     tableRow.appendChild(tableData);
+  } 
 
-  }
-
-
-  for(i = 0; i < myLibrary.length; i++ ){
-
-    if(myLibrary[i] != null){
-
-      tableWhole.children[i+2].children[0].innerHTML = myLibrary[i].title;
-      tableWhole.children[i+2].children[1].innerHTML = myLibrary[i].author;
-      tableWhole.children[i+2].children[2].innerHTML = myLibrary[i].pages;
-      tableWhole.children[i+2].children[3].innerHTML = myLibrary[i].alreadyRead;
-
-    
-    
-  }
-}
-  
 // Delete single entry
   let deleteButton = document.createElement("button");
   deleteButton.setAttribute("id", "delButton");
+  tableRow.appendChild(deleteButton);
 
-  tableRow.children[4].appendChild(deleteButton);
 
   deleteButton.addEventListener("click", () => {
 
     let rowIndex = document.querySelector(`[data-index="${itemIndex}"]`);
     tableWhole.removeChild(rowIndex);
-    myLibrary[itemIndex] = "";
+    delete myLibrary[itemIndex];
 
-    myLibrary.clean("");
+    // myLibrary.clean(null);
 
   });
+}
+
+// Reset Button
+resetButton.addEventListener("click", ()=> {
+
+  location.reload();
+
+});
+
+// close form window
+body.addEventListener('click', () => {
+  if(isHidden(formWhole) == false) {
+      if (!formWhole.contains(event.target) && 
+              event.target !== addButton) {
+                formWhole.classList.remove('show');
+                formWhole.classList.add('hide');
+      }
+  }
+});
+
+function isHidden(e) {
+  return e.offsetParent == null;
 }
